e27e168c229f2f38abeadb7298701030fad0d811
Juliana Lemos <lemos.julianajesus@gmail.com>
2025-06-14 10:43:57 -0500

    week 06_ problems 01 and 02

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,22 @@ public class Node
     {
         // TODO Start Problem 1
 
+        // Update the Insert function of the Node class to
+        // only allow unique values to be added to the tree
+        // (thus creating a sorted set). 
+        // The Insert function is already written to correctly
+        // insert values into the tree. However, the current
+        // implementation will cause duplicate values to be a
+        // dded to the tree.
+
+        // what I have to do is basically add a condition that checks 
+        // if the value already exists or not
+
+        if (value == Data)
+        {
+            return;
+        }
+
         if (value < Data)
         {
             // Insert to the left
@@ -34,8 +50,46 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        // Implement the Contains function in the Node class. 
+        // This function is called by the Contains function 
+        // in the BinarySearchTree to search for a value in the tree.
+        // If the value is found, true should be returned;
+        // otherwise return false. 
+        // Hint: study the Insert function.
+        // You will need to use recursion to solve this problem.
+
+        // If we need to use recursion, let's start with the base case 
+        // and the small problem.
+
+        // Base case: If the current node contains the value, return true.
+        // Also, if  the current node is null, return false.
+
+        // Small problem: Search one side of the tree.
+
+        //  The keyword "this" to refer to
+        // the current instance of the Node class
+        if (this == null) // Base case. (if  the current node is null, return false.))
+        {
+            return false;
+        }
+
+        if (value == Data) // Base case. (If the current node contains the value, return true).
+        {
+            return true;
+        }
+        if (value < Data) // recursion
+        {
+            // Calls Contains(value) on the left child, if it exists.
+             return Left?.Contains(value) ?? false;
+        }
+        else
+        {
+            // Does the same thing, but on the right child.
+            return Right?.Contains(value) ?? false; 
+        }
+        
     }
+
 
     public int GetHeight()
     {
